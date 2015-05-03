02ab6767046692a48dc8393ea8635160f4dc9231
02122993 <xujiazhen522@163.com>
2015-05-03 16:37:19 +0800

    del singlelink.h

--- Link/singlelink.h
+++
@@ -1,8 +0,0 @@
-#include "type.h"
-
-SingleLinkNode *new_single_link_node(int const id);
-SingleLink *new_single_link();
-int append_single_node(SingleLink *link, SingleLinkNode *node);
-int insert_single_node_after(SingleLink *link, SingleLinkNode *node, int const after_id);
-int insert_single_node_before(SingleLink *link, SingleLinkNode *node, int const before_id);
-int delete_single_node(SingleLink *link, int const id);
